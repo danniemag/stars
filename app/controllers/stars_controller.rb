c6ad5a25b36ca96aff457cd0087fe62b18b15eca
Dannie Magalhaes <danniemagalhaes@gmail.com>
2023-03-11 23:29:44 -0300

    [Stars] Prevent from injections and undesirable characters

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,5 +1,6 @@
 class StarsController < ApplicationController
   before_action :no_user_found, if: -> { params[:user].blank? }
+  before_action :sanitize_entry, if: -> { params[:username].present? }
 
   def index
     CheckStarsJob.perform_async(params[:user])
@@ -11,6 +12,13 @@ class StarsController < ApplicationController
 
   def no_user_found
     render_response(false, 'No user found', :not_found)
+  end
+
+  def sanitize_entry
+    # Prevent against injections and/or undesirable characters
+    return if params[:username].index(/[^[:alnum:]&[-]&[_]]/).nil?
+
+    render_response(false, 'Abnormal user name', :bad_request)
   end
 
   def render_response(success, message, status = :ok)
